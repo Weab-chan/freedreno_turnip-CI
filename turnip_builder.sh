83b10e97e84c11d8570e06212684051fae10b200
Weab chan <93067085+Weab-chan@users.noreply.github.com>
2025-09-09 00:15:14 -0400

    switcharoo

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -12,10 +12,10 @@ mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
 
 #array of string => commit/branch;patch args
 base_patches=(
-	"disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;"
+	"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
 experimental_patches=(
-	"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
+	"disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;"
 )
 failed_patches=()
 commit=""
