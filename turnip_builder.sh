a20f6adf6be3eb2dae1781c4c8d6bda8f97120db
Weab chan <93067085+Weab-chan@users.noreply.github.com>
2025-10-11 19:44:34 -0400

    begone

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -15,7 +15,7 @@ base_patches=(
 	"force_sysmem_no_autotuner;../../patches/force_sysmem_no_autotuner.patch;"
 )
 experimental_patches=(
-	"disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;"
+	# "disable_VK_KHR_workgroup_memory_explicit_layout;../../patches/disable_KHR_workgroup_memory_explicit_layout.patch;"
 )
 failed_patches=()
 commit=""
